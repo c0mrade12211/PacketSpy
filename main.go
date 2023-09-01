b19b0b9de14bc1ce16143731b58239abc938f10e
Daniil Stepanov <132468035+c0mrade12211@users.noreply.github.com>
2023-09-01 10:34:00 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,49 +5,50 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"time"
 	"strings"
 	"syscall"
+	"time"
+	"net"
 	"github.com/common-nighthawk/go-figure"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/pcapgo"
 	"golang.org/x/term"
+	"runtime"
 )
 
 func OpenWireshark() {
 	fileName := "captured.pcap"
-	cmd := exec.Command("wireshark", fileName)
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "start", "wireshark", fileName)
+	} else {
+		cmd = exec.Command("wireshark", fileName)
+	}
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
 func ExitAndOpenWireshark() {
 	fmt.Println("Exiting program...")
 	OpenWireshark()
 	os.Exit(0)
 }
 
-
-func hello_function(){
+func hello_function() {
 	figure := figure.NewFigure("Give Me Packets", "", true)
-
 	figure.Print()
 	fmt.Println(" -> Created by c0mrade <-")
 }
 
-func main() {
-
-	hello_function()
-
-	time.Sleep(100 * time.Millisecond)
+func CapturePackets() {
 	devices, err := pcap.FindAllDevs()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println("[----------------------------------------------------------------]")
 	fmt.Println("[+] Available devices")
 	for i, device := range devices {
@@ -75,11 +76,9 @@ func main() {
 
 	w := pcapgo.NewWriter(file)
 	w.WriteFileHeader(65536, layers.LinkTypeEthernet)
-
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	fmt.Println("Press 'q' to exit and open Wireshark")
-
 	termios, err := term.MakeRaw(int(syscall.Stdin))
 	if err != nil {
 		log.Fatal(err)
@@ -99,9 +98,94 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		if strings.ToLower(string(b)) == "q" {
 			ExitAndOpenWireshark()
 		}
+	}
+}
+
+func SendPcapToServer() {
+	var server_ip string
+	var server_port int
+
+	fmt.Print("[+] Enter server IP: \n")
+	_, err := fmt.Scanln(&server_ip)
+	if err != nil {
+		fmt.Println("[-] Invalid server IP")
+		log.Fatal(err)
+	}
+	fmt.Print("[+] Enter server port: \n")
+	_, err = fmt.Scanf("%d", &server_port)
+	if err != nil {
+		fmt.Println("[-] Error: Port")
+		log.Fatal(err)
+	}
+	fmt.Println("[+] Connecting to server...")
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", server_ip, server_port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	file, err := os.Open("captured.pcap")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileSize := make([]byte, 8)
+	fileSize[0] = byte(fileInfo.Size() >> 56)
+	fileSize[1] = byte(fileInfo.Size() >> 48)
+	fileSize[2] = byte(fileInfo.Size() >> 40)
+	fileSize[3] = byte(fileInfo.Size() >> 32)
+	fileSize[4] = byte(fileInfo.Size() >> 24)
+	fileSize[5] = byte(fileInfo.Size() >> 16)
+	fileSize[6] = byte(fileInfo.Size() >> 8)
+	fileSize[7] = byte(fileInfo.Size())
+
+	_, err = conn.Write(fileSize)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buffer := make([]byte, 4096)
+	for {
+		bytesRead, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		_, err = conn.Write(buffer[:bytesRead])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("[+] File sent successfully!")
+
+}
+
+func main() {
+	hello_function()
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Println("[1] Capture Packets and save to file (and open Wireshark)")
+	fmt.Println("[2] Capture Packets and send to server")
+
+	var choice int
+	_, err := fmt.Scanln(&choice)
+	if err != nil || (choice != 1 && choice != 2) {
+		log.Fatal("Invalid choice")
+	}
+
+	if choice == 1 {
+		CapturePackets()
+	} else if choice == 2 {
+		SendPcapToServer()
 	}
 }
